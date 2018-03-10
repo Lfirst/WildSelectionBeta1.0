b12880cfe5fb6438ccab03c43b548b38389f74c8
Lfirst <lulu101m@yahoo.fr>
2018-03-10 11:42:58 +0100

    Dossier a assets
    Tous les assets du jeu

---
+++ Menu/Assets/PlayerMotor.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+    public Camera cam;
+    private Vector3 velocityM = Vector3.zero;
+    private Vector3 rotationM = Vector3.zero;
+    private Vector3 CameraRotationM = Vector3.zero;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Get a movement Vector
+
+    public void Move(Vector3 velocityC)
+    {
+        velocityM = velocityC;
+    }
+
+    // Get a rotational Vector
+
+    public void Rotate(Vector3 rotationC)
+    {
+        rotationM = rotationC;
+    }
+
+    // Get a camera rotational Vector
+
+    public void RotateCamera(Vector3 CameraRotationC)
+    {
+        CameraRotationM = CameraRotationC;
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    //Perform movement based on velocty variable
+    void PerformMovement()
+    {
+        if (velocityM != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocityM * Time.fixedDeltaTime);
+        }
+    }
+
+    //Perform rotation
+    void PerformRotation()
+    {
+
+        rb.MoveRotation(rb.rotation* Quaternion.Euler(rotationM));
+
+        if (cam != null)
+        {
+            cam.transform.Rotate(CameraRotationM);
+            
+        }
+    }
+
+
+}
---
+++ Menu/Assets/PlayerNController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerNController : MonoBehaviour {
+
+    public float speed = 5f;
+    public float lookSensitivity = 3f;
+    public float gravity = 10;
+    private PlayerMotor motor;
+
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+    private void Update()
+    {
+        //Calculate movement velocity as a 3D vector
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float zMove = Input.GetAxisRaw("Vertical");
+        float yMove = Input.GetAxisRaw("Jump");
+
+        Vector3 moveHorizontal = transform.right * xMove;
+        Vector3 moveVertical = transform.forward * zMove;
+        Vector3 moveUp = transform.up * (yMove-gravity) ;
+
+        //final movement vector
+        Vector3 velocityC = (moveHorizontal + moveVertical).normalized * speed;
+
+        //Apply movement
+        motor.Move(velocityC);
+
+        //Calculate rotation as a 3d vector (turning around)
+        float yRotC = Input.GetAxisRaw("Mouse X");
+
+        //Only rotation on y for the player and the camera (Vertical)
+        Vector3 rotationC = new Vector3(0f, yRotC, 0f) * lookSensitivity;
+
+        //Apply rotation
+            motor.Rotate(rotationC);
+
+        //Calculate camera rotation as a 3d vector 
+        float xRotC = Input.GetAxisRaw("Mouse Y");
+        //Only rotation on y for the camera (Horizontal)
+        Vector3 CameraRotationC = new Vector3(xRotC,0f,  0f) * lookSensitivity;
+
+        //Apply camera rotation ( There is a - before camera rotation to inverse axe x)
+        motor.RotateCamera(-CameraRotationC);
+
+
+
+
+
+    }
+
+}
